02b9d690160effe058f9c081066c24248cd1bbcf
Andrew Johnson <ajohnson@draster.com>
2014-12-25 20:59:20 -0800

    Basic graphics implementation

--- handmadeqt.cpp
+++ handmadeqt.cpp
@@ -1,10 +1,20 @@
 #include "handmadeqt.h"
+#include "handmade/code/handmade.h"
 #include <QApplication>
 #include <QElapsedTimer>
 #include <QObject>
+#include <QScreen>
+#include <QPainter>
+
+global_variable bool32 GlobalRunning;
+global_variable bool32 GlobalPause;
+
+global_variable QImage *qImgBuffer;
+
+extern "C" GAME_UPDATE_AND_RENDER(GameUpdateAndRender);
 
 HandmadeQt::HandmadeQt(QWidget *parent)
-    : QMainWindow(parent), didClose(false)
+    : QMainWindow(parent)
 {
 }
 
@@ -15,7 +25,13 @@ HandmadeQt::~HandmadeQt()
 
 void HandmadeQt::closeEvent(QCloseEvent *)
 {
-    didClose = true;
+    GlobalRunning = false;
+}
+
+void HandmadeQt::paintEvent(QPaintEvent *paintEvent) {
+    QPainter painter(this);
+    QRectF rect(0.0, 0.0, (qreal)qImgBuffer->width(), (qreal)qImgBuffer->height());
+    painter.drawImage(rect, *qImgBuffer, rect);
 }
 
 int main(int argc, char *argv[])
@@ -25,17 +41,66 @@ int main(int argc, char *argv[])
 
     w.show();
 
-    while(true) {
-        QElapsedTimer timer;
-        timer.start();
-        QCoreApplication::processEvents(QEventLoop::AllEvents, 5);
+    int MonitorRefreshHz = (int)a.primaryScreen()->refreshRate();
+    printf("%i\n", MonitorRefreshHz);
+    if(MonitorRefreshHz <= 1) {
+        MonitorRefreshHz = 60;
+    }
+    real32 GameUpdateHz = (MonitorRefreshHz / 2.0f);
+    real32 TargetSecondsPerFrame = 1.0f / (real32)GameUpdateHz;
 
-        if(w.didClose) {
-            break;
+    qImgBuffer = new QImage(1280, 720, QImage::Format_RGBX8888);
+
+    game_memory GameMemory = {};
+    GameMemory.PermanentStorageSize = Megabytes(64);
+    GameMemory.TransientStorageSize = Gigabytes(1);
+    GameMemory.DEBUGPlatformFreeFileMemory = 0;
+    GameMemory.DEBUGPlatformReadEntireFile = 0;
+    GameMemory.DEBUGPlatformWriteEntireFile = 0;
+
+    size_t TotalSize = GameMemory.PermanentStorageSize + GameMemory.TransientStorageSize;
+    GameMemory.PermanentStorage = (void*) new char[ TotalSize ];
+    GameMemory.TransientStorage = ((uint8 *)GameMemory.PermanentStorage + GameMemory.PermanentStorageSize);
+
+    GlobalRunning = true;
+    GlobalPause = false;
+
+    game_input Input[2] = {};
+    game_input *NewInput = &Input[0];
+    game_input *OldInput = &Input[1];
+
+    QElapsedTimer frameTimer;
+    frameTimer.start();
+    while(GlobalRunning) {
+        if(!GlobalPause) {
+            thread_context Thread = {};
+
+            game_offscreen_buffer Buffer = {};
+            Buffer.Memory = qImgBuffer->bits();
+            Buffer.Width = qImgBuffer->width();
+            Buffer.Height = qImgBuffer->height();
+            Buffer.Pitch = qImgBuffer->bytesPerLine();
+            Buffer.BytesPerPixel = qImgBuffer->depth() / 8;
+
+            GameUpdateAndRender(&Thread, &GameMemory, NewInput, &Buffer);
+
+            w.update();
+            QCoreApplication::processEvents(QEventLoop::AllEvents, 5);
+
+            real32 SecondsElapsedForFrame = NSTOS(frameTimer.nsecsElapsed());
+            //qDebug( "secsperframe: %f\n", SecondsElapsedForFrame );
+            if(SecondsElapsedForFrame < TargetSecondsPerFrame) {
+                while(SecondsElapsedForFrame < TargetSecondsPerFrame) {
+                    SecondsElapsedForFrame = NSTOS(frameTimer.nsecsElapsed());
+                }
+            } else {
+                // Missed frame
+                //qDebug( "missed frame!!\n" );
+            }
+
+            real32 MSPerFrame = NSTOMS(frameTimer.nsecsElapsed());
+            frameTimer.restart();
+            //qDebug( "MSPerFrame: %f\n", MSPerFrame );
         }
-
-        //printf( "elapsed: %i\n", timer.elapsed() );
-        timer.restart();
-        while(!timer.hasExpired(15));
     }
 }
--- handmadeqt.h
+++ handmadeqt.h
@@ -8,11 +8,14 @@ class HandmadeQt : public QMainWindow
     Q_OBJECT
 
 public:
-    bool didClose;
     HandmadeQt(QWidget *parent = 0);
     ~HandmadeQt();
 protected:
     void closeEvent(QCloseEvent *event);
+    void paintEvent(QPaintEvent *);
 };
+
+#define NSTOS(x) ((real64)x / 1000000000.0)
+#define NSTOMS(x) ((real64)x / 1000000.0)
 
 #endif // HANDMADEQT_H
